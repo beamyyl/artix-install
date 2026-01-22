c95f8d84e66343e858412c770d072aeb06833789
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 08:57:32 +0200

    Create artix-bios-s6-installer.sh

---
+++ artix-bios-s6-installer.sh
@@ -0,0 +1,54 @@
+# ----------------------------------------------------------
+# made by beamyyl
+# This is the BIOS Artix s6 installer.
+# ----------------------------------------------------------
+#!/bin/bash
+set -e
+
+echo ">>> Ensure your root partition is marked as 'Bootable' and mounted to /mnt."
+sleep 3
+
+# 1. Install Base System
+echo ">>> Installing base system with basestrap..."
+basestrap /mnt base base-devel s6 linux linux-firmware
+
+# 2. Generating FSTAB
+echo ">>> Generating fstab..."
+fstabgen -U /mnt >> /mnt/etc/fstab
+
+# 3. Enter chroot
+artix-chroot /mnt /bin/bash <<'EOF'
+export PS1="(artix-s6) ${PS1}"
+
+# Localization
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "artix" > /etc/hostname
+
+# Networking & Utilities
+pacman -S --noconfirm networkmanager-s6 connman-s6 vim nano cronie-s6
+
+# Enabling services
+s6-rc-bundle add default networkmanager
+s6-rc-bundle add default cronie
+
+# ----------------------------------------------------------
+# Bootloader
+# ----------------------------------------------------------
+pacman -S --noconfirm grub
+
+grub-install \
+  --target=i386-pc \
+  /dev/sda
+
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+# 4. Root password
+echo ">>> Set root password"
+artix-chroot /mnt /bin/bash -c 'passwd'
+
+echo "=================================================="
+echo " Artix s6 BIOS installation complete!"
+echo "=================================================="
