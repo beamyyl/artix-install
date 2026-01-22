3c78224533fb1be0530cd9320c8e48685df7a2c7
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 09:14:33 +0200

    Update artix-bios-dinit-installer.sh

--- artix-bios-dinit-installer.sh
+++ artix-bios-dinit-installer.sh
@@ -5,50 +5,27 @@
 #!/bin/bash
 set -e
 
-echo ">>> Ensure your root partition is marked as 'Bootable' and mounted to /mnt."
-sleep 3
-
-# 1. Install Base System
-echo ">>> Installing base system with basestrap..."
-basestrap /mnt base base-devel dinit linux linux-firmware
-
-# 2. Generating FSTAB
-echo ">>> Generating fstab..."
+basestrap /mnt base base-devel dinit elogind-dinit linux linux-firmware
 fstabgen -U /mnt >> /mnt/etc/fstab
 
-# 3. Enter chroot
 artix-chroot /mnt /bin/bash <<'EOF'
 export PS1="(artix-dinit) ${PS1}"
 
-# Localization
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "artix" > /etc/hostname
 
 # Networking & Utilities
 pacman -S --noconfirm networkmanager-dinit connman-dinit vim nano cronie-dinit
-
-# Enabling services
+# dinit service linking as per Wiki
 ln -s ../networkmanager /etc/dinit.d/boot.d/
 ln -s ../cronie /etc/dinit.d/boot.d/
 
-# ----------------------------------------------------------
-# Bootloader
-# ----------------------------------------------------------
 pacman -S --noconfirm grub
-
-grub-install \
-  --target=i386-pc \
-  /dev/sda
-
+grub-install --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-# 4. Root password
-echo ">>> Set root password"
 artix-chroot /mnt /bin/bash -c 'passwd'
-
-echo "=================================================="
-echo " Artix dinit BIOS installation complete!"
-echo "=================================================="
