135df675b2a5b094bfa78fa7f3c2d4d1be7eea76
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 09:12:46 +0200

    Update artix-uefi-s6-installer.sh

--- artix-uefi-s6-installer.sh
+++ artix-uefi-s6-installer.sh
@@ -5,52 +5,26 @@
 #!/bin/bash
 set -e
 
-echo ">>> Ensure disks are mounted to /mnt and /mnt/boot."
-sleep 3
-
-# 1. Install Base System
-echo ">>> Installing base system with basestrap..."
-basestrap /mnt base base-devel s6 linux linux-firmware
-
-# 2. Generating FSTAB
-echo ">>> Generating fstab..."
+basestrap /mnt base base-devel s6-base elogind-s6 linux linux-firmware
 fstabgen -U /mnt >> /mnt/etc/fstab
 
-# 3. Enter chroot
 artix-chroot /mnt /bin/bash <<'EOF'
 export PS1="(artix-s6) ${PS1}"
 
-# Localization
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "artix" > /etc/hostname
 
 # Networking & Utilities
 pacman -S --noconfirm networkmanager-s6 connman-s6 vim nano cronie-s6
+touch /etc/s6/adminsv/default/contents.d/networkmanager
+touch /etc/s6/adminsv/default/contents.d/cronie
 
-# Enabling services in s6
-# (Artix s6 uses the s6-rc-bundle command to enable services)
-s6-rc-bundle add default networkmanager
-s6-rc-bundle add default cronie
-
-# ----------------------------------------------------------
-# Bootloader
-# ----------------------------------------------------------
 pacman -S --noconfirm grub efibootmgr
-
-grub-install \
-  --target=x86_64-efi \
-  --efi-directory=/boot \
-  --bootloader-id=Artix
-
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-# 4. Root password
-echo ">>> Set root password"
 artix-chroot /mnt /bin/bash -c 'passwd'
-
-echo "=================================================="
-echo " Artix s6 UEFI installation complete!"
-echo "=================================================="
