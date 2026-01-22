7d1ee19a6952be88c6c54b391dc1f19fd0481faa
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 08:54:10 +0200

    Create artix-bios-openrc-installer.sh

---
+++ artix-bios-openrc-installer.sh
@@ -0,0 +1,52 @@
+# ----------------------------------------------------------
+# made by beamyyl
+# This is the BIOS Artix OpenRC installer.
+# ----------------------------------------------------------
+#!/bin/bash
+set -e
+
+echo ">>> Ensure your root partition is marked as 'Bootable' and mounted to /mnt."
+sleep 3
+
+# 1. Install Base System
+echo ">>> Installing base system with basestrap..."
+basestrap /mnt base base-devel openrc linux linux-firmware
+
+# 2. Generating FSTAB
+echo ">>> Generating fstab..."
+fstabgen -U /mnt >> /mnt/etc/fstab
+
+# 3. Enter chroot
+artix-chroot /mnt /bin/bash <<'EOF'
+export PS1="(artix) ${PS1}"
+
+# Localization
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "artix" > /etc/hostname
+
+# Networking & Utilities
+pacman -S --noconfirm networkmanager-openrc connman-openrc vim nano cronie-openrc
+rc-update add NetworkManager default
+rc-update add cronie default
+
+# ----------------------------------------------------------
+# Bootloader
+# ----------------------------------------------------------
+pacman -S --noconfirm grub
+
+grub-install \
+  --target=i386-pc \
+  /dev/sda
+
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+# 4. Root password
+echo ">>> Set root password"
+artix-chroot /mnt /bin/bash -c 'passwd'
+
+echo "=================================================="
+echo " Artix OpenRC BIOS installation complete!"
+echo "=================================================="
