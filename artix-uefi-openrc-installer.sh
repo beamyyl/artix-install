1ac14d232cd2c970e8753b5b65c08cbeb3537233
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 08:53:28 +0200

    Create artix-uefi-openrc-installer.sh

---
+++ artix-uefi-openrc-installer.sh
@@ -0,0 +1,53 @@
+# ----------------------------------------------------------
+# made by beamyyl
+# This is the UEFI Artix OpenRC installer.
+# ----------------------------------------------------------
+#!/bin/bash
+set -e
+
+echo ">>> Ensure disks are mounted to /mnt and /mnt/boot."
+sleep 3
+
+# 1. Install Base System
+echo ">>> Installing base system with basestrap..."
+basestrap /mnt base base-devel openrc linux linux-firmware
+
+# 2. Generating FSTAB
+echo ">>> Generating fstab..."
+fstabgen -U /mnt >> /mnt/etc/fstab
+
+# 3. Enter chroot
+artix-chroot /mnt /bin/bash <<'EOF'
+export PS1="(artix) ${PS1}"
+
+# Localization
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "artix" > /etc/hostname
+
+# Networking & Utilities
+pacman -S --noconfirm networkmanager-openrc connman-openrc vim nano cronie-openrc
+rc-update add NetworkManager default
+rc-update add cronie default
+
+# ----------------------------------------------------------
+# Bootloader
+# ----------------------------------------------------------
+pacman -S --noconfirm grub efibootmgr
+
+grub-install \
+  --target=x86_64-efi \
+  --efi-directory=/boot \
+  --bootloader-id=Artix
+
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+# 4. Root password
+echo ">>> Set root password"
+artix-chroot /mnt /bin/bash -c 'passwd'
+
+echo "=================================================="
+echo " Artix OpenRC UEFI installation complete!"
+echo "=================================================="
