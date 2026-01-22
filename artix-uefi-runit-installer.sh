16ef384c79e16592c7571a48cff5a9c7ad6a6da5
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 09:12:01 +0200

    Update artix-uefi-runit-installer.sh

--- artix-uefi-runit-installer.sh
+++ artix-uefi-runit-installer.sh
@@ -5,51 +5,29 @@
 #!/bin/bash
 set -e
 
-echo ">>> Ensure disks are mounted to /mnt and /mnt/boot."
+echo ">>> Ensure disks are mounted to /mnt and /mnt/boot/efi."
 sleep 3
 
-# 1. Install Base System
-echo ">>> Installing base system with basestrap..."
-basestrap /mnt base base-devel runit linux linux-firmware
-
-# 2. Generating FSTAB
-echo ">>> Generating fstab..."
+basestrap /mnt base base-devel runit elogind-runit linux linux-firmware
 fstabgen -U /mnt >> /mnt/etc/fstab
 
-# 3. Enter chroot
 artix-chroot /mnt /bin/bash <<'EOF'
 export PS1="(artix-runit) ${PS1}"
 
-# Localization
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "artix" > /etc/hostname
 
 # Networking & Utilities
 pacman -S --noconfirm networkmanager-runit connman-runit vim nano cronie-runit
+ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default
+ln -s /etc/runit/sv/cronie /etc/runit/runsvdir/default
 
-# Enabling services in Runit (symlinking to /etc/runit/runsvdir/default)
-ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default/
-ln -s /etc/runit/sv/cronie /etc/runit/runsvdir/default/
-
-# ----------------------------------------------------------
-# Bootloader
-# ----------------------------------------------------------
 pacman -S --noconfirm grub efibootmgr
-
-grub-install \
-  --target=x86_64-efi \
-  --efi-directory=/boot \
-  --bootloader-id=Artix
-
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-# 4. Root password
-echo ">>> Set root password"
 artix-chroot /mnt /bin/bash -c 'passwd'
-
-echo "=================================================="
-echo " Artix Runit UEFI installation complete!"
-echo "=================================================="
