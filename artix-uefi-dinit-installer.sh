cd84de109efe72f8877b5c236225e97c76c358db
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-01-22 09:13:01 +0200

    Update artix-uefi-dinit-installer.sh

--- artix-uefi-dinit-installer.sh
+++ artix-uefi-dinit-installer.sh
@@ -5,51 +5,26 @@
 #!/bin/bash
 set -e
 
-echo ">>> Ensure disks are mounted to /mnt and /mnt/boot."
-sleep 3
-
-# 1. Install Base System
-echo ">>> Installing base system with basestrap..."
-basestrap /mnt base base-devel dinit linux linux-firmware
-
-# 2. Generating FSTAB
-echo ">>> Generating fstab..."
+basestrap /mnt base base-devel dinit elogind-dinit linux linux-firmware
 fstabgen -U /mnt >> /mnt/etc/fstab
 
-# 3. Enter chroot
 artix-chroot /mnt /bin/bash <<'EOF'
 export PS1="(artix-dinit) ${PS1}"
 
-# Localization
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "artix" > /etc/hostname
 
 # Networking & Utilities
 pacman -S --noconfirm networkmanager-dinit connman-dinit vim nano cronie-dinit
-
-# Enabling services in dinit
 ln -s ../networkmanager /etc/dinit.d/boot.d/
 ln -s ../cronie /etc/dinit.d/boot.d/
 
-# ----------------------------------------------------------
-# Bootloader
-# ----------------------------------------------------------
 pacman -S --noconfirm grub efibootmgr
-
-grub-install \
-  --target=x86_64-efi \
-  --efi-directory=/boot \
-  --bootloader-id=Artix
-
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-# 4. Root password
-echo ">>> Set root password"
 artix-chroot /mnt /bin/bash -c 'passwd'
-
-echo "=================================================="
-echo " Artix dinit UEFI installation complete!"
-echo "=================================================="
